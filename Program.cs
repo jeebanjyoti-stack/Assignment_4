ddc812cfefee0dd81f709dfc709fcc17d479c290
jeeban <jeebanjyoti193@gmail.com>
2023-07-13 14:30:50 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int number=Convert.ToInt32(Console.ReadLine());
+            if (number > 0)
+            {
+                Console.WriteLine("The Number is Possitive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The Number is Negative");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Zero");
+            }
+            string NumberType = number % 2 == 0 ? "even" : "odd";
+            Console.WriteLine("The number is " + NumberType + ".");
+
+
+
+        }
+    }
+}
